a2ddd19b09949246bf17f08b22ae00fc0081609e
Kayla <harris.kaylamarie@gmail.com>
2023-09-29 19:34:16 -0500

    added some comments

--- Lesson 3/FunctionExamples/power.py
+++ Lesson 3/FunctionExamples/power.py
@@ -4,11 +4,16 @@
 def power(num1, num2):
     # return num1**num2 #Technically this is valid but i want to practice writing out functions
     ret_val = 1
+    nums_left = num2
+    # for num in range(num2):
+    #     ret_val = ret_val * num1
+    # return ret_val
 
-    for num in range(num2):
-        ret_val = ret_val * num1
+    while nums_left > 0:
+        ret_val *= num1  # ret_val = ret_val * num1
+        nums_left -= 1  # nums_left = nums_left - 1
     return ret_val
 
 
-print(power(5, 3))
-print(5**3)
+print(power(8, 54))
+print(8**54)
